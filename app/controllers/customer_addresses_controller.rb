b76c12aa8ff9c47edcc866c0ede39044137ff36f
Matthew Blott <m4ttb@pm.me>
2024-03-14 14:04:02 +0000

    Made the customer address code tidier

--- app/controllers/customer_addresses_controller.rb
+++ app/controllers/customer_addresses_controller.rb
@@ -1,6 +1,5 @@
 class CustomerAddressesController < ApplicationController
-  before_action :set_customer, only: %i[show edit update]
-  before_action :set_delete_customer_address, only: %i[destroy]
+  before_action :set_address, only: %i[show edit]
 
   def index
     @customer = Customer.find(customer_id)
@@ -11,28 +10,18 @@ class CustomerAddressesController < ApplicationController
 
   def new
     @address = Address.new
-    # @customer = Customer.find(customer_id)
-    # @customer.addresses.build
   end
 
   def edit
   end
 
   def create
-    @customer = Customer.find(customer_id)
-    @customer.addresses.create(
-      :name => params[:name],
-      :address_line_1 => params[:address_line_1],
-      :address_line_2 => params[:address_line_2],
-      :postal_town => params[:postal_town],
-      :country => params[:country],
-      :post_code => params[:post_code]
-    )
-    if @customer.save
+    customer = Customer.find(customer_id)
+    address = customer.addresses.create(address_params)
 
-      address_id = @customer.addresses.last.id
+    address_id = address.id
 
-      # render(:new, status: :see_other)
+    if address_id != nil
       redirect_to(
         show_customer_address_path(customer_id, address_id),
         notice: "Address was successfully created.",
@@ -44,7 +33,8 @@ class CustomerAddressesController < ApplicationController
   end
 
   def update
-    if @customer.update(customer_params)
+    customer = Customer.find(customer_id)
+    if customer.addresses.update(address_params)
       redirect_to(
         show_customer_address_path(customer_id, address_id),
         notice: "Address was successfully updated.",
@@ -56,7 +46,8 @@ class CustomerAddressesController < ApplicationController
   end
 
   def destroy
-    @customer.addresses.find(address_id).destroy
+    customer = Customer.find(customer_id)
+    customer.addresses.find(address_id).destroy
     redirect_to(index_customer_address_path, notice: "Address was successfully destroyed.", status: :see_other)
   end
 
@@ -68,20 +59,6 @@ class CustomerAddressesController < ApplicationController
 
   def address_id
     params[:address_id]
-  end
-
-  def set_customer
-    @customer = Customer
-      .eager_load(:addresses)
-      .where("customer_id = '#{customer_id}'")
-      .where(
-        "address_id = #{address_id}"
-      )
-      .first
-  end
-
-  def set_delete_customer_address
-    @customer = Customer.find(customer_id)
   end
 
   def address_params
@@ -96,18 +73,9 @@ class CustomerAddressesController < ApplicationController
     )
   end
 
-  def customer_params
-    customer_params = params.require(:customer).permit(
-      addresses_attributes: [
-        :id,
-        :address_line_1,
-        :address_line_2,
-        :postal_town,
-        :county,
-        :country,
-        :post_code
-      ]
-    )
+  def set_address
+    customer = Customer.find(customer_id)
+    @address = customer.addresses.find(address_id)
   end
 
 end
